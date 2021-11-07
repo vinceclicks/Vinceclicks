76cf0f5da743f75403d81c267cd6691f1055b29d
Vincent Thomason <p.vincent.thomason@gmail.com>
2021-11-07 13:01:32 -0500

    add projects section. nav active class fix

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -1,55 +1,58 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("#navbarToggler li a");
 
-const sections = document.querySelectorAll('section');
-const navLi = document.querySelectorAll('#navbarToggler li a')
-
-window.addEventListener('scroll', ()=> {
+window.addEventListener("scroll", () => {
     let current = "";
-    sections.forEach( section => {
+    sections.forEach((section) => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
-        if(scrollY >= (sectionTop - sectionHeight/3)){
-            current = section.getAttribute('id');
+        if (scrollY >= sectionTop - sectionHeight / 2.2) {
+            current = section.getAttribute("id");
         }
-    })
-    navLi.forEach( a => {
-        a.classList.remove('active');
-        if(a.classList.contains(current)){
-            a.classList.add('active')
+    });
+    navLi.forEach((a) => {
+        a.classList.remove("active");
+        if (a.classList.contains(current)) {
+            a.classList.add("active");
         }
-    })
-})
+    });
+});
 
-
-window.addEventListener('scroll'||'click', ()=> {
-    navLi.forEach( a => {
-        const navText= a.textContent;
-        if (a.classList.contains('active')){
-            if(a.textContent == "Home"){
-                a.textContent = "<Home/>"
+window.addEventListener("scroll" || "click", () => {
+    navLi.forEach((a) => {
+        const navText = a.textContent;
+        if (a.classList.contains("active")) {
+            if (a.textContent == "Home") {
+                a.textContent = "<Home/>";
             }
-            if(a.textContent == "About"){
-                a.textContent = "<About/>"
+            if (a.textContent == "About") {
+                a.textContent = "<About/>";
             }
-            if(a.textContent == "Skills"){
-                a.textContent = "<Skills/>"
+            if (a.textContent == "Skills") {
+                a.textContent = "<Skills/>";
             }
-            if(a.textContent == "Contact"){
-                a.textContent = "<Contact/>"
+            if (a.textContent == "Projects") {
+                a.textContent = "<Projects/>";
+            }
+            if (a.textContent == "Contact") {
+                a.textContent = "<Contact/>";
+            }
+        } else {
+            if (a.textContent == "<Home/>") {
+                a.textContent = "Home";
+            }
+            if (a.textContent == "<About/>") {
+                a.textContent = "About";
+            }
+            if (a.textContent == "<Skills/>") {
+                a.textContent = "Skills";
+            }
+            if (a.textContent == "<Projects/>") {
+                a.textContent = "Projects";
+            }
+            if (a.textContent == "<Contact/>") {
+                a.textContent = "Contact";
             }
         }
-        else {
-            if(a.textContent == "<Home/>"){
-                a.textContent = "Home"
-            }
-            if(a.textContent == "<About/>"){
-                a.textContent = "About"
-            }
-            if(a.textContent == "<Skills/>"){
-                a.textContent = "Skills"
-            }
-            if(a.textContent == "<Contact/>"){
-                a.textContent = "Contact"
-            }
-        }
-        })
-})
+    });
+});
